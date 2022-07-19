00179ee202e8279008798b2201e9fddd8e7d0a93
Fellipe Neves <fellipe2.0e@gmail.com>
2022-07-19 17:13:29 -0300

    Started Signin screen

---
+++ App.tsx
@@ -0,0 +1,29 @@
+
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import React from 'react';
+
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
+import Signin from './src/screens/Signin';
+import { THEME } from './src/styles/theme';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, 
+    Roboto_700Bold
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>  
+      <StatusBar 
+        barStyle='light-content'        
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Signin/> : <Loading/>}      
+    </NativeBaseProvider>
+  );
+}
+
