4bc83870508d7bf10b118b01fe0c137b9e3303dd
Fellipe Neves <fellipe2.0e@gmail.com>
2022-07-20 08:59:11 -0300

    Finished first day

---
+++ Readme.md
@@ -0,0 +1,33 @@
+# Rockethelp
+
+Aplicação construida durante o evento Ignite Lab, utilizando React Native e Expo.
+
+<br/>
+
+## Como executar o projeto? 🤔
+
+Após clonar o repositório, instale todas as dependências utilizando o comando:
+```
+npm install
+```
+ou 
+```
+yarn
+```
+
+Com o expo instalado em sua máquina, execute o comando
+```
+expo start
+```
+Feito isso, você poderá executar o app em seu dispositivo fisico ou emulador de sua preferência.
+
+## Tecnologias e conceitos
+- React Native;
+- Expo e Bare Workflow;
+- Expo fonts;
+- Typescript;
+- Native base;
+- Utilização de estados e conceito de imutabilidade;
+- React Native SVG;
+- React Native SVG Transformer;
+- Criação de componentes dinamicos;
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,27 +1,42 @@
 import { Heading, VStack, Icon, useTheme } from 'native-base';
-import React from 'react';
-import {Envelope, Key} from "phosphor-react-native";
+import React, { useState } from 'react';
+import {Envelope, Eye, Key} from "phosphor-react-native";
 import Logo from '../assets/logo_primary.svg';
+
 import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 export default function Signin(){
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    
     const {colors} = useTheme();
+
+    function handleSignin(){
+
+    }
+
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
             <Logo />
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-                Acesse sua conta
+                Acesse sua conta {name}
             </Heading>
 
             <Input 
                 placeholder="E-mail"
                 mb={4}
                 InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={4}/>}
+                onChangeText={setName}
             />
             <Input 
+                mb={8}
                 placeholder="Senha"
                 InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4}/>}
+                secureTextEntry
+                onChangeText={setPassword}                
             />
+            <Button title="Entrar" w="full" onPress={handleSignin}/>
         </VStack>
     )
 }
