099e6b816625f1df3500b54e9c33bfdf1b62579a
Fellipe Neves <fellipe2.0e@gmail.com>
2022-07-20 17:04:07 -0300

    fix to run on ios

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -6,28 +6,34 @@ import Logo from '../assets/logo_primary.svg';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
+import auth from '@react-native-firebase/auth'
+import { Alert } from 'react-native';
+
 export default function Signin(){
-    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     
     const {colors} = useTheme();
 
     function handleSignin(){
 
+        if(!email || !password){
+            return Alert.alert('Erro', 'E-mail e/ou senha inválidos')
+        }
     }
 
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
             <Logo />
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-                Acesse sua conta {name}
+                Acesse sua conta
             </Heading>
 
             <Input 
                 placeholder="E-mail"
                 mb={4}
                 InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={4}/>}
-                onChangeText={setName}
+                onChangeText={setEmail}
             />
             <Input 
                 mb={8}
